598fb75e34fd5297f374aa9501a093a9ad12729b
greengerong <grzx2210@163.com>
2014-09-22 21:38:13 +0800

    [refactor] remove unused BookImageOnScrollListener

--- app/src/main/java/com/github/greengerong/book/PlaceholderFragment.java
+++ app/src/main/java/com/github/greengerong/book/PlaceholderFragment.java
@@ -6,23 +6,22 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AbsListView;
 import android.widget.ListView;
 
-import com.github.greengerong.book.adapter.BookImageOnScrollListener;
 import com.github.greengerong.book.service.GetBooksAsyncTask;
 import com.github.greengerong.book.service.ViewHelper;
 
 /**
- * A placeholder fragment containing a simple view.
+ * ***************************************
+ * *
+ * Auth: green gerong                     *
+ * Date: 2014                             *
+ * blog: http://greengerong.github.io/    *
+ * github: https://github.com/greengerong *
+ * *
+ * ****************************************
  */
 public class PlaceholderFragment extends Fragment {
-
-    private final AbsListView.OnScrollListener onScrollListener;
-
-    public PlaceholderFragment() {
-        onScrollListener = new BookImageOnScrollListener();
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -31,7 +30,6 @@ public class PlaceholderFragment extends Fragment {
         final Activity context = getActivity();
         final ListView bookList = new ViewHelper(rootView).findViewById(R.id.bookList);
         new GetBooksAsyncTask(bookList, context).execute("https://api.douban.com/v2/book/search?q=%E7%BC%96%E7%A8%8B");
-//        bookList.setOnScrollListener(onScrollListener);
         return rootView;
     }
 
