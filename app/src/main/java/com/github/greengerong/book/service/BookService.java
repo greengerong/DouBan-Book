fce9e38c7a24a3ff407eeca7151cad6cbfadf832
greengerong <grzx2210@163.com>
2014-09-27 22:57:21 +0800

    [refactor] url encode

--- app/src/main/java/com/github/greengerong/book/service/BookService.java
+++ app/src/main/java/com/github/greengerong/book/service/BookService.java
@@ -1,9 +1,13 @@
 package com.github.greengerong.book.service;
 
+import android.util.Log;
+
 import com.github.greengerong.book.domain.BookSearchResult;
+import com.github.greengerong.book.utils.LogUtils;
 import com.github.greengerong.book.utils.delegate.Action;
 import com.github.greengerong.book.utils.delegate.Action1;
 
+import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
 
 /**
@@ -20,9 +24,15 @@ public class BookService {
     private static final String BOOK_API_URL = "https://api.douban.com/v2/book/search?q=%s&start=%s&count=%s";
     private static final String BOOK_TAG = "大数据";
     private static final int PAGE_SIZE = 20;
+    private static final String TAG = BookService.class.getName();
 
     public static String getBookApiUrl(int start) {
-        return String.format(BOOK_API_URL, URLEncoder.encode(BOOK_TAG), start, PAGE_SIZE);
+        try {
+            return String.format(BOOK_API_URL, URLEncoder.encode(BOOK_TAG, "UTF-8"), start, PAGE_SIZE);
+        } catch (UnsupportedEncodingException e) {
+            Log.e(TAG, LogUtils.getStackTrace(e));
+            throw new RuntimeException(e);
+        }
     }
 
     public void getBookList(Action onPreExecuteListener, Action1<BookSearchResult> onPostExecuteListener, int start) {
